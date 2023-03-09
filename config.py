0ff4625bcdc64eb21fd5be4fd2219dca5c88042b
ssMuss <correoadfr@gmail.com>
2023-03-09 00:22:34 -0600

    agregando iconos

--- config.py
+++ config.py
@@ -42,58 +42,68 @@ keys = [
     # A list of available commands that can be bound to keys can be found
     # at https://docs.qtile.org/en/latest/manual/config/lazy.html
     # Switch between windows
-    Key([mod], "l", lazy.layout.left(), desc="Move focus to left"),
-    Key([mod], "j", lazy.layout.right(), desc="Move focus to right"),
-    Key([mod], "k", lazy.layout.down(), desc="Move focus down"),
-    Key([mod], "i", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+    Key([mod], "l",     lazy.layout.left(),             desc="Move focus to left"),
+    Key([mod], "j",     lazy.layout.right(),            desc="Move focus to right"),
+    Key([mod], "k",     lazy.layout.down(),             desc="Move focus down"),
+    Key([mod], "i",     lazy.layout.up(),               desc="Move focus up"),
+    Key([mod], "n",     lazy.layout.normalize(),        desc="Reset all window sizes"),
+    Key([mod], "w",     lazy.window.kill(),             desc="Kill focused window"),
+    Key([mod], "r",     lazy.spawncmd(),                desc="Spawn a command using a prompt widget"),
+    Key([mod], "m",     lazy.next_screen(),             desc='Next monitor'),
+    Key([mod], "o",     lazy.spawn("rofi -show run")),
+    Key([mod], "s",     lazy.spawn("scrot")),
+    Key([mod], "e",     lazy.spawn("thunar")),
+    Key([mod], "s",     lazy.spawn("spotify")),
+    Key([mod], "space", lazy.layout.next(),             desc="Move window focus to other window"),
+    
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "l", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_right(), desc="Move window to the right"),
-    Key([mod, "shift"], "k", lazy.layout.shuffle_down(), desc="Move window down"),
-    Key([mod, "shift"], "i", lazy.layout.shuffle_up(), desc="Move window up"),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_left(),    desc="Move window to the left"),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_right(),   desc="Move window to the right"),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_down(),    desc="Move window down"),
+    Key([mod, "shift"], "i", lazy.layout.shuffle_up(),      desc="Move window up"),
+    Key([mod, 'shift'], "s", lazy.spawn("scrot -s")),
+    Key([mod, 'shift'], "m", lazy.spawn("rofi -show")),
+    Key([mod, 'shift'], "l", lazy.spawn("betterlockscreen -l")),
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
-    Key([mod, "control"], "l", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "j", lazy.layout.grow_right(), desc="Grow window to the right"),
-    Key([mod, "control"], "k", lazy.layout.grow_down(), desc="Grow window down"),
-    Key([mod, "control"], "i", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+    Key([mod, "control"], "l", lazy.layout.grow_left(),   desc="Grow window to the left"),
+    Key([mod, "control"], "j", lazy.layout.grow_right(),  desc="Grow window to the right"),
+    Key([mod, "control"], "k", lazy.layout.grow_down(),   desc="Grow window down"),
+    Key([mod, "control"], "i", lazy.layout.grow_up(),     desc="Grow window up"),
+    Key([mod, "control"], "q", lazy.shutdown(),           desc="Shutdown Qtile"),
+    Key([mod, "control"], "r", lazy.reload_config(),      desc="Reload the config"),
+    
+   
    
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
     # multiple stack panes
+    Key([mod],"Return", lazy.spawn(terminal),   desc="Launch terminal"),
+    Key([mod], "Tab",   lazy.next_layout(),     desc="Toggle between layouts"),
     Key(
         [mod, "shift"],
         "Return",
         lazy.layout.toggle_split(),
         desc="Toggle between split and unsplit sides of stack",
     ),
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-   # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
-    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
-    Key([mod], "m", lazy.next_screen(), desc='Next monitor'),
-    Key([mod], "o", lazy.spawn("rofi -show run")),
-    Key([mod, 'shift'], "m", lazy.spawn("rofi -show")),
-    Key([mod], "s", lazy.spawn("scrot")),
-    Key([mod, 'shift'], "s", lazy.spawn("scrot -s")),
-    Key([mod], "e", lazy.spawn("thunar")),
-    Key([mod, 'shift'], "l", lazy.spawn("betterlockscreen -l")),
-
+   # Toggle between different layouts as defined below 
 ]
+groups = []
 
+# FOR QWERTY KEYBOARDS
+group_names = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"]
+# group_labels = ["", "", "", "", "", "", "", "", "", "",]
+group_labels = ["", "", "", " ", "4", "5", "6", "7", "8", "9"]
 
-groups = [Group(i) for i in "123456789"]
-
-# groups = [Group(i) for i in [
-#     "   ", "   ", "   ", "   ", "  ", "   ", "   ", "   ", "   ",
-# ]]
+for i in range(len(group_names)):
+    groups.append(
+        Group(
+            name=group_names[i],
+            layout=group_layouts[i].lower(),
+            label=group_labels[i],
+        ))
 
 for i in groups:
     keys.extend(
@@ -136,9 +146,9 @@ layouts = [
 ]
 
 widget_defaults = dict(
-    font="sans,Symbols Nerd Font Mono",
-    fontsize=12,
-    padding=3,
+    font="FreeMono Regular",
+    fontsize=14,
+    padding=4,
 )
 extension_defaults = widget_defaults.copy()
 
